bca53abe848aa99a8f13a9711fe05d89cb318794
Fabian <amazon@fmgrafikdesign.de>
2023-04-19 01:10:35 +0200

    Bugfixes, Improvements, cool stuff.

--- interessenten-management/src/views/InteressentView.vue
+++ interessenten-management/src/views/InteressentView.vue
@@ -1,10 +1,10 @@
 <script setup>
-import Input from "@/components/Input.vue";
-import { useRouter } from "vue-router";
-import { computed } from "vue";
-import { InteressentenStatus, usePropertyStore } from "@/stores/property-store";
-import { useSnackbarStore } from "@/stores/snackbar-store";
-import Button from "@/components/Button.vue";
+import Input from '@/components/Input.vue'
+import { useRouter } from 'vue-router'
+import { computed } from 'vue'
+import { InteressentenStatus, usePropertyStore } from '@/stores/property-store'
+import { useSnackbarStore } from '@/stores/snackbar-store'
+import Button from '@/components/Button.vue'
 
 const props = defineProps({
   propertyId: {
@@ -36,85 +36,100 @@ const removeInteressent = async () => {
 
 // Update Interessenten last edited on changes to interessent
 properties.$subscribe((mutation) => {
-  if (mutation.events.target?.lastEdited) {
-    mutation.events.target.lastEdited = new Date()
-  }
+    if (mutation.events.target?.lastEdited) {
+        mutation.events.target.lastEdited = new Date()
+    }
 })
 
 const snackbar = useSnackbarStore()
-const savePropert;y = async () => {
-  await properties.persistProperty(property)
-  snackbar.showSn;ackbar({
-    text: 'Interessent gespe"Interessent gespeichert!"ateNe;wI;nteressent = () => {
-  const interessent = properties.addInteressent(property.value)
-  goToInteressent(interessent)
+const saveProperty = async () => {
+    await properties.persistProperty(property)
+    snackbar.showSnackbar({
+        text: 'Interessent gespeichert'
+    })
+}
+
+const createNewInteressent = () => {
+    const interessent = properties.addInteressent(property.value)
+    goToInteressent(interessent)
 }
 
 function goToInteressent(interessent) {
-  return router.replace({
-    name: "interessent",
-    params: {
-      propertyId: property.value.id,
-      interessentenId: interessent.id
+    return router.replace({
+        name: 'interessent',
+        params: {
+            propertyId: property.value.id,
+            interessentenId: interessent.id
     }
   })
 }
 </script>
 
 <template>
-  <div class="interessent-view w-full flex flex-col h-full">
-    <div class="top-navigation flex justify-between p-2 border-b border-b-gray-300">
-      <button class="back-button flex items-center gap-1 hover:text-gray-500" @click="router.back">
-        <icon:material-symbols:arrow-back />
-        Zurück
-      </button>
-      <h1 class="text-xl">Interessent</h1>
-      <button class="save-button flex items-center gap-1" @click="saveProperty">
-        Speichern
-        <icon:mdi:content-save />
-      </button>
-    </div>
+    <div class='interessent-view w-full flex flex-col h-full'>
+        <div class='top-navigation flex justify-between p-2 border-b border-b-gray-300'>
+            <button
+                class='back-button flex items-center gap-1 hover:text-gray-500'
+                @click='router.back'
+            >
+                <icon:material-symbols:arrow-back />
+                Zurück
+            </button>
+            <h1 class='text-xl'>Interessent</h1>
+            <button class='save-button flex items-center gap-1' @click='saveProperty'>
+                Speichern
+                <icon:mdi:content-save />
+            </button>
+        </div>
 
-    <div class="interessent-content flex-grow bg-gray-200 flex flex-col px-2 pt-4">
-      <template v-if="interessent">
-        <div class="grid grid-cols-2 gap-2 mb-4">
-          <Input v-model="interessent.firstName" placeholder="Vorname"></Input>
-          <Input v-model="interessent.surname" placeholder="Nachname"></Input>
+        <div class='interessent-content flex-grow bg-gray-200 flex flex-col px-2 pt-4'>
+            <template v-if='interessent'>
+                <div class='grid grid-cols-2 gap-2 mb-4'>
+                    <Input v-model='interessent.firstName' placeholder='Vorname'></Input>
+                    <Input v-model='interessent.surname' placeholder='Nachname'></Input>
 
-          <Input v-model="interessent.phone" placeholder="Telefonnummer" class="col-span-2"></Input>
-          <Input v-model="interessent.notes" placeholder="Notizen" class="col-span-2"></Input>
+                    <Input
+                        v-model='interessent.phone'
+                        placeholder='Telefonnummer'
+                        class='col-span-2'
+                    ></Input>
+                    <Input
+                        v-model='interessent.notes'
+                        placeholder='Notizen'
+                        class='col-span-2'
+                    ></Input>
+                </div>
+                <div class='interessent-status flex flex-col gap-1'>
+                    <h3 class='text-lg'>Status: {{ interessent.status }}</h3>
+                    <div
+                        v-for='(value, key) in InteressentenStatus'
+                        :key='key'
+                        class='radio flex items-center py-1 gap-2'
+                    >
+                        <input
+                            type='radio'
+                            v-model='interessent.status'
+                            :id='key'
+                            :value='value'
+                            class='rounded-full w-4 h-4'
+                        />
+                        <label :for='key'>{{ value }}</label>
+                    </div>
+                </div>
+                <div class='remove-interessent-button-wrapper mx-auto'>
+                    <button
+                        @click='removeInteressent'
+                        class='w-auto inline-block mt-8 underline text-red-500'
+                    >
+                        Interessent löschen
+                    </button>
+                </div>
+            </template>
         </div>
-        <div class="interessent-status flex flex-col gap-1">
-          <h3 class="text-lg">Status: {{ interessent.status }}</h3>
-          <div
-            v-for="(value, key) in InteressentenStatus"
-            :key="key"
-            class="radio flex items-center py-1 gap-2"
-          >
-            <input
-              type="radio"
-              v-model="interessent.status"
-              :id="key"
-              :value="value"
-              class="rounded-full w-4 h-4"
-            />
-            <label :for="key">{{ value }}</label>
-          </div>
+        <div class='add-property-section border-t p-4 border-t-gray-300'>
+            <Button @click='createNewInteressent'>weiteren Interessenten anlegen</Button>
         </div>
-        <div class="remove-interessent-button-wrapper mx-auto">
-          <button
-            @click="removeInteressent"
-            class="w-auto inline-block mt-8 underline text-red-500"
-          >
-            Interessent löschen
-          </button>
-        </div>
-      </template>
     </div>
-    <div class="add-property-section border-t p-4 border-t-gray-300">
-      <Button @click="createNewInteressent">weiteren Interessenten anlegen</Button>
-    </div>
-  </div>
 </template>
 
 <style scoped></style>
--- interessenten-management/src/views/PropertyView.vue
+++ interessenten-management/src/views/PropertyView.vue
@@ -1,128 +1,130 @@
-<script setup lang="ts">
-import { useRouter } from "vue-router";
-import Input from "@/components/Input.vue";
-import { computed, ref } from "vue";
-import type { Interessent } from "@/stores/property-store";
-import { usePropertyStore } from "@/stores/property-store";
-import SearchBar from "@/components/SearchBar.vue";
-import InteressentListItem from "@/components/InteressentListItem.vue";
-import Button from "@/components/Button.vue";
-import { useSnackbarStore } from "@/stores/snackbar-store";
+<script lang='ts' setup>
+import { useRouter } from 'vue-router'
+import Input from '@/components/Input.vue'
+import { computed, ref } from 'vue'
+import type { Interessent } from '@/stores/property-store'
+import { usePropertyStore } from '@/stores/property-store'
+import SearchBar from '@/components/SearchBar.vue'
+import InteressentListItem from '@/components/InteressentListItem.vue'
+import Button from '@/components/Button.vue'
+import { useSnackbarStore } from '@/stores/snackbar-store'
 
 const props = defineProps({
-  propertyId: {
-    required: true,
-    type: String
-  }
+    propertyId: {
+        required: true,
+        type: String
+    }
 })
 
 const router = useRouter()
 const properties = usePropertyStore()
 
 const property = computed(() =>
-  properties.all.find((propertyToCheck) => props.propertyId === propertyToCheck.id)
+    properties.all.find((propertyToCheck) => props.propertyId === propertyToCheck.id)
 )
 
 const removeProperty = async () => {
-  router.push({
-    name: 'index'
-  })
-  await properties.removeProperty(property)
-  const snackbar = useSnackbarStore()
-  snackbar.showSnackbar({ text: 'Immobilie gelöscht.' })
+    router.push({
+        name: 'index'
+    })
+    await properties.removeProperty(property)
+    const snackbar = useSnackbarStore()
+    snackbar.showSnackbar({ text: 'Immobilie gelöscht.' })
 }
 
 const searchCriteria = ref('')
 const filteredInteressenten = computed(() =>
-  property.value.interessenten
-    .filter(
-      (interessent) =>
-        interessent.firstName.toUpperCase().includes(searchCriteria.value.toUpperCase()) ||
-        interessent.surname.toUpperCase().includes(searchCriteria.value.toUpperCase())
-    )
-    .sort((a, b) => {
-      const first = a.surname.toUpperCase()
-      const second = b.surname.toUpperCase()
-      return first < second ? -1 : first > second ? 1 : 0
-    })
+    property.value.interessenten
+        .filter(
+            (interessent) =>
+                interessent.firstName.toUpperCase().includes(searchCriteria.value.toUpperCase()) ||
+                interessent.surname.toUpperCase().includes(searchCriteria.value.toUpperCase())
+        )
+        .sort((a, b) => {
+            const first = a.surname.toUpperCase()
+            const second = b.surname.toUpperCase()
+            return first < second ? -1 : first > second ? 1 : 0
+        })
 )
 
 const createNewInteressent = () => {
-  const interessent = properties.addInteressent(property.value)
-  goToInteressent(interessent)
+    const interessent = properties.addInteressent(property.value)
+    goToInteressent(interessent)
 }
 
 function goToInteressent(interessent: Interessent) {
-  return router.push({
-    name: 'interessent',
-    params: {
-      propertyId: property.value.id,
-      interessentenId: interessent.id
-    }
-  })
+    return router.push({
+        name: 'interessent',
+        params: {
+            propertyId: property.value.id,
+            interessentenId: interessent.id
+        }
+    })
 }
 
 const snackbar = useSnackbarStore()
-const saveProperty = async; () => {
-  await properties.persistProperty(property)
-  snackbar.showSnackbar({
-;    text: 'Immobilie gespeichert!'
-  "Immobilie gespeichert!"e>
-  ;<d;iv class="property-view w-full flex flex-col h-full">
-    <div class="top-navigation flex justify-between p-2 border-b border-b-gray-300">
-      <button
-        class="back-button flex items-center gap-1 hover:text-gray-500"
-        @click="router.push({ name: 'index' })"
-      >
-        <icon-material-symbols:arrow-back />
-        Zurück
-      </button>
-      <h1 class="text-xl">Immobilie</h1>
-      <button class="save-button flex items-center gap-1 hover:text-gray-500" @click="saveProperty">
-        Speichern
-        <icon-mdi:content-save />
-      </button>
-    </div>
+const saveProperty = async () => {
+    await properties.persistProperty(property)
+    snackbar.showSnackbar({ text: 'Immobilie gespeichert!' })
+}
+</script>
 
-    <div class="property-content flex-grow bg-gray-200 flex flex-col px-2 pt-4">
-      <template v-if="property">
-        <div class="property-title">
-          <Input v-model="property.name" placeholder="Name der Immobilie"></Input>
+<template>
+    <div class='property-view w-full flex flex-col h-full'>
+        <div class='top-navigation flex justify-between p-2 border-b border-b-gray-300'>
+            <button
+                class='back-button flex items-center gap-1 hover:text-gray-500'
+                @click="router.push({ name: 'index' })"
+            >
+                <icon-material-symbols:arrow-back />
+                Zurück
+            </button>
+            <h1 class='text-xl'>Immobilie</h1>
+            <button class='save-button flex items-center gap-1 hover:text-gray-500' @click='saveProperty'>
+                Speichern
+                <icon-mdi:content-save />
+            </button>
         </div>
 
-        <div class="interessenten-section flex-grow">
-          <h2 class="text-center text-xl mt-4 mb-2">Interessenten</h2>
-          <template v-if="property.interessenten.length">
-            <SearchBar v-model="searchCriteria" placeholder="Nach Interessenten suchen" />
-            <template v-if="filteredInteressenten.length">
-              <div class="property-list flex-grow bg-gray-200 flex flex-col">
-                <InteressentListItem
-                  v-for="interessent in filteredInteressenten"
-                  :key="interessent.id"
-                  :interessent="interessent"
-                  @click="goToInteressent(interessent)"
-                  class="mt-2"
-                />
-              </div>
+        <div class='property-content flex-grow bg-gray-200 flex flex-col px-2 pt-4'>
+            <template v-if='property'>
+                <div class='property-title'>
+                    <Input v-model='property.name' placeholder='Name der Immobilie'></Input>
+                </div>
+
+                <div class='interessenten-section flex-grow'>
+                    <h2 class='text-center text-xl mt-4 mb-2'>Interessenten</h2>
+                    <template v-if='property.interessenten.length'>
+                        <SearchBar v-model='searchCriteria' placeholder='Nach Interessenten suchen' />
+                        <template v-if='filteredInteressenten.length'>
+                            <div class='property-list flex-grow bg-gray-200 flex flex-col'>
+                                <InteressentListItem
+                                    v-for='interessent in filteredInteressenten'
+                                    :key='interessent.id'
+                                    :interessent='interessent'
+                                    class='mt-2'
+                                    @click='goToInteressent(interessent)'
+                                />
+                            </div>
+                        </template>
+                        <template v-else>
+                            <p class='text-center mt-4'>Keine Interessenten entsprechen diesen Kriterien</p>
+                        </template>
+                    </template>
+                    <template v-else>
+                        <p class='text-center text-lg p-2'>Hier erscheinen deine Interessenten</p>
+                    </template>
+                </div>
+                <div class='remove-property-button-wrapper mx-auto'>
+                    <button class='w-auto inline-block mb-8 underline text-red-500' @click='removeProperty'>
+                        Immobilie löschen
+                    </button>
+                </div>
             </template>
-            <template v-else>
-              <p class="text-center mt-4">Keine Interessenten entsprechen diesen Kriterien</p>
-            </template>
-          </template>
-          <template v-else>
-            <p class="text-center text-lg p-2">Hier erscheinen deine Interessenten</p>
-          </template>
         </div>
-        <div class="remove-property-button-wrapper mx-auto">
-          <button @click="removeProperty" class="w-auto inline-block mb-8 underline text-red-500">
-            Immobilie löschen
-          </button>
-        </div>
-      </template>
-    </div>
 
-    <div class="add-property-section border-t p-4 border-t-gray-300">
-      <Button @click="createNewInteressent">Interessent anlegen</Button>
+        <div class='add-property-section border-t p-4 border-t-gray-300'>
+            <Button @click='createNewInteressent'>Interessent anlegen</Button>
+        </div>
     </div>
-  </div>
 </template>
